7fb3a03310434dd753811781e2eebbbfeee8729d
rome10 <rome10@ROME2-6.BT-Training.be>
2016-08-22 12:24:48 +0200

    ajout de main

---
+++ AidooProject/src/main/java/be/bt/controllers/SimpleClass.java
@@ -0,0 +1,12 @@
+package be.bt.controllers;
+
+import org.apache.log4j.chainsaw.Main;
+
+public class SimpleClass {
+	
+	public static void main(String[] args) {
+		System.out.println("Aidoo Git");
+	}
+
+
+}
